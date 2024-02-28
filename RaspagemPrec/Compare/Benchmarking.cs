92a363ded92387dcd3093676786ebcd5f8ef6e1a
JPacademico <ximbas@academico.ufs.br>
2024-02-28 17:32:06 -0300

    Adicionar arquivos de projeto.

---
+++ RaspagemPrec/Compare/Benchmarking.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspagemPrec.Compare
+{
+    internal class Benchmarking
+    {
+        public static string Compare(string nome, int idproduto)
+        {
+
+            string precoLivre;
+            string precoLuiza;
+            char[] charsToTrim = { 'R', '$', ' ' };
+
+            MercadoLivreScraper mercado = new();
+            MagazineLuizaScraper magazine = new();
+
+            precoLivre = mercado.ObterPreco(nome, idproduto);
+            precoLuiza = magazine.ObterPreco(nome, idproduto);
+
+            decimal livreDecimal = Convert.ToDecimal(precoLivre.Trim(charsToTrim));
+            decimal luizaDecimal = Convert.ToDecimal(precoLuiza.Trim(charsToTrim));
+
+
+            if (luizaDecimal > livreDecimal)
+            {
+
+                return $"No mercado livre está R${luizaDecimal - livreDecimal} mais barato";
+            }
+            else if(luizaDecimal < livreDecimal)
+            {
+
+                return $"No magazine Luiza está R${livreDecimal - luizaDecimal} mais barato";
+            }
+            else
+            {
+                return "Preço igual";
+            }
+        }
+    }
+}
---
+++ RaspagemPrec/Operations/SendLink.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspagemPrec.Operations
+{
+    internal class SendLink
+    {
+        //Enviar email com o resultado da comparação
+        static void EnviarEmail(string nome, string precoLuiza, string precoLivre, string responseCompare)
+        {
+            // Configurações do servidor SMTP do Gmail
+            string smtpServer = "smtp-mail.outlook.com"; // Servidor SMTP do Gmail
+            int porta = 587; // Porta SMTP do Gmail para TLS/STARTTLS
+            string remetente = "wallacemaximus@hotmail.com"; // Seu endereço de e-mail do Gmail
+            string senha = "teste"; // Sua senha do Gmail
+
+            // Configurar cliente SMTP
+            using (SmtpClient client = new SmtpClient(smtpServer, porta))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(remetente, senha);
+                client.EnableSsl = true; // Habilitar SSL/TLS
+
+                // Construir mensagem de e-mail
+                MailMessage mensagem = new MailMessage(remetente, "wallace@docente.senai.br")
+                {
+                    Subject = "Resultado da comparação de preços",
+                    Body = $"Mercado Livre\n" +
+                           $"Produto: {nome}\n" +
+                           $"Preço: {precoLivre}\n" +
+                           "" +
+                           $"Mercado Livre\n" +
+                           $"Produto: {nome}\n" +
+                           $"Preço: {precoLuiza}\n" +
+                           "" +
+                           $"{responseCompare}\n" +
+                           ""
+
+                };
+
+                // Enviar e-mail
+                client.Send(mensagem);
+
+
+            }
+
+
+        }
+    }
+
+    
+}
